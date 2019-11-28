aa582e7b4a60003ada500b599bf83b498d8bc12f
Jecyu <jecyu.lin@gmail.com>
2019-11-28 09:31:09 +0800

    📝2019-11-28 09:31:09

--- deploy.sh
+++ deploy.sh
@@ -25,6 +25,6 @@ git commit -m '🎉deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Jecyu/Fe-Auto-Testing.git master:gh-pages
+git push -f git@github.com:Jecyu/JS-Event-Loop.git master:gh-pages
 
 cd -
